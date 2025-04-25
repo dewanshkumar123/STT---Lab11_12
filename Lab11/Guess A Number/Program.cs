dc2b4dcd2b94dc1ab8dfb62d7969ef0c18325c08
dewanshkumar123 <dewansh.kumar@iitgn.ac.in>
2025-04-25 23:04:31 +0530

    Lab11 Codes

---
+++ Lab11/Guess A Number/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+int value = Random.Shared.Next(1, 101);
+while (true)
+{
+	Console.Write("Guess a number (1-100): ");
+	bool valid = int.TryParse((Console.ReadLine() ?? "").Trim(), out int input);
+	if (!valid) Console.WriteLine("Invalid.");
+	else if (input == value) break;
+	else Console.WriteLine($"Incorrect. Too {(input < value ? "Low" : "High")}.");
+}
+Console.WriteLine("You guessed it!");
+Console.Write("Press any key to exit...");
+Console.ReadKey(true);
+
+
+//int value = Random.Shared.Next(1, 101);
+
+//while (true)
+//{
+//	Console.Write("Guess a number (1-100): ");
+//	string? inputStr = Console.ReadLine();
+//	if (string.IsNullOrWhiteSpace(inputStr))
+//	{
+//		Console.WriteLine("Invalid input. Try again.");
+//		continue;
+//	}
+//	if (inputStr == value.ToString())
+//	{
+//		break; 
+//	}
+
+//	bool valid = int.TryParse(inputStr.Trim(), out int input);
+//	if (!valid)
+//		Console.WriteLine("Invalid number.");
+//	else if (input == value)
+//		break; 
+//	else
+//		Console.WriteLine($"Incorrect. Too {(input < value ? "Low" : "High")}.");
+//}
